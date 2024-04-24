51c8dfe6450d826f14fbaca3cdee0654e0c5f98e
nurullailia20 <nurullailia720@gmail.com>
2024-04-24 16:20:39 +0700

    [style] slicing product page

--- src/components/Productpage/index.jsx
+++ src/components/Productpage/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import ActiveSlider from "./ActiveSlider";
+import Image from "next/image";
+import mobil from "../../../public/assets/mobil.jpg";
 
 function Productpage() {
-  
   return (
-    <>
-      <div>
-       
+    <div className="w-full h-full flex justify-center items-center flex-col gap-y-4">
+      <div className="max-w-[700px] h-[300px] flex justify-center items-center">
+        <ActiveSlider />
       </div>
-    </>
+      <div className="w-[700px] h-[150px] grid grid-cols-3 gap-3">
+        <div className="h-full w-full border"><Image src={mobil}/></div>
+        <div className="h-full w-full border"><Image src={mobil}/></div>
+        <div className="h-full w-full border"><Image src={mobil}/></div>
+      </div>
+    </div>
   );
 }
 
