ec07ecbd3f27d6685e61e6d058887ddaebb82412
nurullailia20 <nurullailia720@gmail.com>
2024-04-24 22:00:22 +0700

    [style] styling hamburger button

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/navigation";
 import React from "react";
+import ButtonBar from "../shared/ButtonBar";
 
 function Navbar() {
   const navlinks = [
@@ -28,13 +29,16 @@ function Navbar() {
   return (
     <nav className="w-full h-[60px] border flex justify-between items-center px-6 py-3">
       <div>Bengkel Variasi Mobil</div>
-      <ul className="w-1/2 flex justify-between gap-2 items-center cursor-pointer">
+      <ul className="w-1/2 justify-between gap-2 items-center cursor-pointer hidden sm:flex">
         {navlinks.map((navlink, index) => (
           <li key={index} onClick={() => router.push(navlink.href)}>
             {navlink.name}
           </li>
         ))}
       </ul>
+      <div className="w-8 h-8 border  sm:hidden">
+        <ButtonBar />
+      </div>
     </nav>
   );
 }
---
+++ src/components/shared/ButtonBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ButtonBar() {
+  return (
+    <div className='w-full h-full flex flex-col justify-around p-1'>
+      <div className='w-full h-[2px] bg-black'/>
+      <div className='w-full h-[2px] bg-black'/>
+      <div className='w-full h-[2px] bg-black'/>
+    </div>
+  )
+}
+
+export default ButtonBar
